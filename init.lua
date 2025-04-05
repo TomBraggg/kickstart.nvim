240ca32593b5f3f78edf73220511e6660b411784
TomBraggg <tom.bragg.197@gmail.com>
2025-04-05 13:20:14 +0100

    python working

--- init.lua
+++ init.lua
@@ -691,7 +691,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = { 'black' },
         go = { 'gofmt', 'goimports' },
         c = { 'clang-format' },
         cpp = { 'clang-format' },
